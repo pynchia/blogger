4e5ed95b1a16d7ea01cad6a73460439f163532ad
MBB <pynchia@gmail.com>
2015-04-13 14:04:01 +0200

    now sending contact email msg

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.core.mail import EmailMessage
 from . import models
+from django.conf import settings
 
 
 class ContactForm(forms.Form):
@@ -8,6 +10,15 @@ class ContactForm(forms.Form):
     name = forms.CharField(required=True, max_length=64)
     email_address = forms.EmailField(required=True, max_length=128)
     message = forms.CharField(required=True, widget=forms.Textarea)
+
+    def send_msg(self):
+        from_email = self.cleaned_data['email_address']
+        email = EmailMessage(subject='Msg from Blog',
+                             cc=[from_email, ],
+                             body=self.cleaned_data['message'],
+                             to=[settings.EMAIL_HOST_USER, ],
+                             headers={'Reply-To': from_email})
+        email.send()
 
 
 class ArticleForm(forms.ModelForm):
