aba910ebcfcc19c2401110534300d634b40371de
MBB <pynchia@gmail.com>
2015-04-21 23:07:04 +0200

    added some tests, finally working since I now include the dump of auth on top of blog as fixtures

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
-
+from .models import Category, Article
 # Create your tests here.
+
+
+class MyTestCase(TestCase):
+
+    fixtures = ['auth.json', 'blog.json', ]
+
+    def setUp(self):
+        self.category = Category.objects.get(pk=2)
+
+    def test_articles_of_category(self):
+        n = self.category.articles.count()
+        self.assertEqual(n, 1)
+        self.category.descr="checczo"
+        self.category.save()
+
+    def test_categories(self):
+        self.assertEqual(self.category.descr, "checczo")
+
