534b580635dc35b283e8e28be633e9f4bb4ec9c6
MBB <pynchia@gmail.com>
2015-04-22 17:55:23 +0200

    testing happily

--- blog/tests.py
+++ blog/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 from .models import Category, Article
 # Create your tests here.
 
@@ -7,15 +8,29 @@ class MyTestCase(TestCase):
 
     fixtures = ['auth.json', 'blog.json', ]
 
+    @classmethod
+    def setUpTestData(cls):
+        # setup data for the whole testcase
+        pass
+
     def setUp(self):
-        self.category = Category.objects.get(pk=2)
+        self.catmusic = Category.objects.get(pk=1)
+        self.catphoto = Category.objects.get(pk=2)
 
-    def test_articles_of_category(self):
-        n = self.category.articles.count()
+    def test_relat_between_articles_and_categories(self):
+        n = self.catmusic.articles.count()
+        self.assertEqual(n, 2)
+        n = self.catphoto.articles.count()
         self.assertEqual(n, 1)
-        self.category.descr="checczo"
-        self.category.save()
 
-    def test_categories(self):
-        self.assertEqual(self.category.descr, "checczo")
+    def test_homepage_redirect_to_blog(self):
+        response = self.client.get(reverse('home'))
+        self.assertRedirects(response, reverse('blog:blog'), status_code=301)
 
+    def test_blog_page_has_three_articles(self):
+        response = self.client.get(reverse('blog:blog'))
+        # the page exists and is returned
+        self.assertEqual(response.status_code, 200)
+        # there should be three articles visible on the page
+        numart = len(response.context['object_list'])
+        self.assertEqual(numart, 3)
