db64eb8765a498a3bc4a663e956e91d5a6e86a2a
MBB <pynchia@gmail.com>
2015-04-23 14:19:40 +0200

    now showing available categories on the sidebar, through a smooth mixin

--- blog/views.py
+++ blog/views.py
@@ -6,17 +6,25 @@ from django.views.generic.detail import SingleObjectMixin
 from django.db.models import Q
 from django.contrib.auth.models import User
 from . import forms
-from .models import Article
+from .models import Category, Article
 
 
-class BlogView(ListView):
+class CategoriesMixin(object):
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoriesMixin, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all().order_by('name')
+        return context
+
+
+class BlogView(CategoriesMixin, ListView):
     # model = Article
     queryset = Article.objects.order_by('-published_on')
     template_name = "blog/blog.html"
     paginate_by = Article.NUM_ARTICLES_IN_PAGE
 
 
-class AuthorArticlesView(SingleObjectMixin, ListView):
+class AuthorArticlesView(CategoriesMixin, SingleObjectMixin, ListView):
     # model = Article
     template_name = "blog/authorarticles.html"
     paginate_by = Article.NUM_ARTICLES_IN_PAGE
@@ -36,7 +44,20 @@ class AuthorArticlesView(SingleObjectMixin, ListView):
     #     return context
 
 
-class SearchArticlesView(ListView):
+class CategArticlesView(CategoriesMixin, SingleObjectMixin, ListView):
+    # model = Article
+    template_name = "blog/categarticles.html"
+    paginate_by = Article.NUM_ARTICLES_IN_PAGE
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=Category.objects.all())
+        return super(CategArticlesView, self).get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        return self.object.articles.order_by('-published_on')
+
+
+class SearchArticlesView(CategoriesMixin, ListView):
     # model = Article
     #template_name = "blog/searcharticles.html"
     template_name = "blog/blog.html"
@@ -51,11 +72,11 @@ class SearchArticlesView(ListView):
         return Article.objects.filter(*queryargs).order_by('-published_on')
 
 
-class StatsView(TemplateView):
+class StatsView(CategoriesMixin, TemplateView):
     template_name = "blog/stats.html"
 
 
-class AboutView(TemplateView):
+class AboutView(CategoriesMixin, TemplateView):
     template_name = "blog/about.html"
 
 
@@ -69,7 +90,7 @@ class ContactView(FormView):
         return super(ContactView, self).form_valid(form)
 
 
-class CreateArticleView(CreateView):
+class CreateArticleView(CategoriesMixin, CreateView):
     form_class = forms.ArticleForm
     template_name = "blog/create_updatearticle.html"
     success_url = reverse_lazy("blog:blog")
@@ -79,7 +100,7 @@ class CreateArticleView(CreateView):
         return super(CreateArticleView, self).form_valid(form)
 
 
-class UpdateArticleView(UpdateView):
+class UpdateArticleView(CategoriesMixin, UpdateView):
     model = Article
     form_class = forms.ArticleForm
     template_name = "blog/create_updatearticle.html"
