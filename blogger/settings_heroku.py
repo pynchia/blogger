2c1fe78748a741260efc74f0d5d378185d25d52d
MBB <pynchia@gmail.com>
2015-09-15 01:30:22 +0200

    stupi error

--- blogger/settings_heroku.py
+++ blogger/settings_heroku.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'HOST': 'ciao',
         'NAME': 'blogger',
-        'USER': 'ciao'],
+        'USER': 'ciao',
         'PASSWORD': 'ciao'],
         'PORT': '',
     }
